2ea075ff8acacce52d4ce41985a39941f67afa61
mook2005 <chananthorn.s@gmail.com>
2025-01-25 10:44:00 +0700

    018 - [add] Setup Project

---
+++ GooseBomb/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+ 
+<template>
+<div>
+  <h1 class="text-2xl font-bold underline">Hello Goose</h1>
+  <h2 class="text-3xl font-bold">World</h2>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
